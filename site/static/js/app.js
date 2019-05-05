519281371a7d99da1f652c746b257884a6ebb3b0
Andrew Archbold <pulsegrenade@gmail.com>
2019-05-05 14:31:17 -0700

    #time 2h 0m adding nav markup, css and javascript

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -68,3 +68,42 @@ function debounce(func, wait, immediate) {
 		if (callNow) func.apply(context, args);
 	};
 };
+
+
+$.fn.handleTopNav = function() {
+    var $context = $(this),
+        $nav = $('.topnav-links-container', $context),
+        $open = $('.topnav-mobile-cta__open', $context),
+        $close = $('.topnav-mobile-cta__close', $context),
+        breakpoint = 860;
+
+    $open.click(function(e) {
+        e.preventDefault();
+        $nav.fadeIn();
+        $open.hide();
+        $close.fadeIn();
+    })
+
+    $close.click(function(e) {
+        e.preventDefault();
+        $nav.fadeOut();
+        $open.fadeIn();
+        $close.hide();
+    })
+
+    var initNav = debounce(function() {        
+        if (window.innerWidth > breakpoint) {  
+            $nav.show();
+        } else {
+            $nav.hide();
+            $close.hide();
+            $open.show();
+        }
+    }, 250);
+
+    window.addEventListener('resize', initNav);
+}
+
+$(function(){
+    $('.topnav-container').handleTopNav();
+})
