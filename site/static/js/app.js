51ab671d4d45cff7493d3bf4fb75eeb4caa47243
Andrew Archbold <pulsegrenade@gmail.com>
2019-05-14 10:49:43 -0700

    Adding better animations

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -70,6 +70,28 @@ function debounce(func, wait, immediate) {
 };
 
 
+$.fn.handleHyperionMap = function() {
+    var $context = $(this),
+        $triggers = $('.home-hyperion__list--item', $context),
+        $panels = $('.hyperion-panel', $context);
+
+    $triggers.click(function() {
+        $triggers.each(function(i,e) {
+            $(e).removeClass('-active');
+        })
+        $panels.each(function(i,e) {
+            $(e).removeClass('-active');
+        })
+        $(this).addClass('-active');
+        $('#' + $(this).attr('data-target')).addClass('-active');
+    });
+}
+
+$(function(){
+    $('.home-hyperion__container').handleHyperionMap();
+})
+
+
 $.fn.handleTopNav = function() {
     var $context = $(this),
         $nav = $('.topnav-links-container', $context),
